c6fd39efe6590b39a7a2148fba6413cbe0e787a1
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-01 11:54:39 +0300

    Маршруты

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from api.views import ListTaskApi, RegisterUserApi, LoginUserApi
+from api.views import ListTaskApi, RegisterUserApi, LoginUserApi, TaskCreate
 
 urlpatterns = [
     path("list-task/", ListTaskApi.as_view()),
+    path("execute-status/<int:task_id>/", ListTaskApi.as_view()),
     path("register-user/", RegisterUserApi.as_view()),
     path("login-user/", LoginUserApi.as_view()),
+    path("create-task/", TaskCreate.as_view()),
 ]
