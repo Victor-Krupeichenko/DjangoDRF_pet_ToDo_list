43ae81cb9d872e4754ad833470458be7820a5367
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-01 11:50:45 +0300

    Разрешения

--- api/permissions.py
+++ api/permissions.py
@@ -2,8 +2,7 @@ from rest_framework import permissions
 
 
 class IsOvnerOnlyRead(permissions.BasePermission):
-    def has_permission(self, request, view):
-        """Разрешаем доступ для всех методов, кроме POST, PUT и DELETE"""
-        if request.method in permissions.SAFE_METHODS:
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:  # Разрешаем доступ для всех методов, кроме POST, PUT и DELETE
             return True
-  
+        return obj.user == request.user  # Разрешает для всех остольных методов
